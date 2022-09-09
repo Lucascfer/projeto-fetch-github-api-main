e67c06e8dae41921391a192857d152dca2d334bb
Lucas Carvalho Fernandes <lucascf000@gmail.com>
2022-09-09 18:20:06 -0300

    validação de informações

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -6,22 +6,37 @@ import { screen } from '/src/scripts/objects/screen.js'
 
 document.getElementById('btn-search').addEventListener('click', () => {
     const userName = document.getElementById('input-search').value
+    if (valideteEmptyImput(userName)) return
     getUserData(userName)
 })
 
 document.getElementById('input-search').addEventListener('keyup', (e) => {
     const userName = e.target.value
     if (e.key === 'Enter') {
+        if (valideteEmptyImput(userName)) return
         getUserData(userName)
     }
 })
 
+function valideteEmptyImput(userName) {
+    if (userName.length === 0) {
+        alert('Preencha o campo com um nome de usuário do GitHub')
+        return true
+    }
+}
+
 async function getUserData(userName) {
     const userResponse = await getUser(userName)
+
+    if (userResponse.message === "Not Found") {
+        screen.renderNotFound()
+        return
+    }
+
     const repositoriesResponse = await getRepositories(userName)
-    
+
     user.setInfo(userResponse)
     user.setRepositories(repositoriesResponse)
-    
+
     screen.renderUser(user)
 }
--- src/scripts/objects/screen.js
+++ src/scripts/objects/screen.js
@@ -8,16 +8,20 @@ const screen = {
                                 <p>${user.bio ?? 'Não possui bio cadastrada 😭'}</p>
                             </div>
                         </div>`
-    
+
         let repositoriesItens = ''
         user.repositories.forEach(repo => repositoriesItens += `<li><a href="${repo.html_url}" target="_blank">${repo.name}</a></li>`)
 
-        if (user.repositories.length > 0){
+        if (user.repositories.length > 0) {
             this.userProfile.innerHTML += `<div class="repositories section">
                                         <h2>Repositórios</h2>
                                         <ul>${repositoriesItens}</ul>
                                         </div>`
         }
+    },
+
+    renderNotFound() {
+        this.userProfile.innerHTML = "<h3>Usuário não encontrado</h3>"
     }
 }
 
